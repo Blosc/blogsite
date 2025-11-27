94489de6a4e4d3d9da0f5341002eadb73b4bf282
Francesc Alted <francesc@blosc.org>
2025-11-27 09:56:52 +0100

    Silence some Nikola warnings

--- conf.py
+++ conf.py
@@ -347,7 +347,8 @@ WRITE_TAG_CLOUD = True
 
 # Generate pages for each section. The site must have at least two sections
 # for this option to take effect. It wouldn't build for just one section.
-POSTS_SECTIONS = True
+# NOTE: Sections feature has been removed in Nikola; use categories instead.
+# POSTS_SECTIONS = True
 
 # Setting this to False generates a list page instead of an index. Indexes
 # are the default and will apply GENERATE_ATOM if set.
@@ -1312,7 +1313,7 @@ BODY_END = ""
 
 # If you hate "Filenames with Capital Letters and Spaces.md", you should
 # set this to true.
-UNSLUGIFY_TITLES = True
+FILE_METADATA_UNSLUGIFY_TITLES = True
 
 # Additional metadata that is added to a post when creating a new_post
 # ADDITIONAL_METADATA = {}
